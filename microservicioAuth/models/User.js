4afe71081ce287dd3f78e1bb6c252561ee13e432
IsmaelJrDev <ismaelbibiano.dev@gmail.com>
2026-04-16 12:05:20 -0600

    feat(auth): add name field to Users table

--- microservicioAuth/models/User.js
+++ microservicioAuth/models/User.js
@@ -3,6 +3,10 @@ const sequelize = require('../config/database')
 
 const User = sequelize.define("User", {
     // Definicion de campos o clumnas
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
