7c211966ef1a35e8c2e2dc39bd4fe2c84850533f
IsmaelJrDev <ismaelbibiano.dev@gmail.com>
2026-04-21 10:48:51 -0600

    refactor(auth): stabilize database connection

--- microservicioAuth/config/database.js
+++ microservicioAuth/config/database.js
@@ -5,10 +5,10 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 // Conexión a la base de datos
-const sequelize = new Sequelize(process.env.URL_CONNECT_P1, {
+const sequelize = new Sequelize(process.env.URL_CONNECT, {
     dialect: 'postgres',
     logging: false,
     pool: {max: 5, min: 0, acquire: 30000, idle: 10000}
 });
 
-module.exports = sequelize;
+module.exports = sequelize; 
